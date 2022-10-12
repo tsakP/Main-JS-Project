3ce81271102667d1f48538aca95042052fb69cdc
Petros Tsakonas <petros-tsakonas@hotmail.com>
2022-10-12 16:41:00 +0300

    Main JS Project Completion
    All 5 objectives were implemented. Additional functionality for game repetition also implemented.

---
+++ script.js
@@ -0,0 +1,66 @@
+// Main JS Project
+
+// 4.b start
+const sComputerPlay = function() {
+    const arGameOptions = ["Rock", "Paper", "Scissors"];
+    let iComputerChoice = Math.floor(Math.random() * 3);
+    return arGameOptions[iComputerChoice];
+}
+// 4.b end
+
+// 4.c & 4.d start
+const sPlayRound = function(playerSelection, computerSelection) {
+    // Making player selection case-insensitive
+    let pSelection = playerSelection.toLowerCase();
+    // Facilitating game logic 
+    let cSelection = computerSelection.toLowerCase();
+
+    // Round Logic
+    if (pSelection == cSelection) return "It's a draw!"
+    if (pSelection == "rock" && cSelection == "paper") return "Computer wins the round! Paper beats Rock!";
+    if (pSelection == "paper" && cSelection == "rock") return "Player wins the round! Paper beats Rock!";
+    if (pSelection == "scissors" && cSelection == "rock") return "Computer wins the round! Rock beats Scissors!";
+    if (pSelection == "rock" && cSelection == "scissors") return "Player wins the round! Rock beats Scissors!";
+    if (pSelection == "paper" && cSelection == "scissors") return "Computer wins the round! Scissors beat paper!";
+    if (pSelection == "scissors" && cSelection == "paper") return "Player wins the round! Scissors beat paper!";
+}
+// 4.c & 4.d end
+
+// 4.e start
+const sGame = function() {
+    // Score variables declaration and initialization
+    let iPlayerScore = 0,
+    iComputerScore = 0;
+
+    // Game Logic
+    for (let i = 0; i < 5; i++) {
+        if (i == 0) console.log("Welcome to RPS! \nGet ready to Rock..., or Paper..., or Scissors!");
+        if (i == 4) console.log("This is the last round!");
+        let sPlayerSelection = prompt("Make your choice: ", "Rock, Paper or Scissors...?");
+        while (sPlayerSelection.toLocaleLowerCase() != "rock" && sPlayerSelection.toLowerCase() != "paper" 
+        && sPlayerSelection.toLowerCase() != "scissors") {
+            sPlayerSelection = prompt("⛔ Invalid input! Please try again!", "Rock, Paper or Scissors...?");
+        }
+        const sComputerSelection = sComputerPlay();
+        let sRoundResult = sPlayRound(sPlayerSelection, sComputerSelection);
+        if (sRoundResult.startsWith("C")) iComputerScore++;
+        if (sRoundResult.startsWith("P")) iPlayerScore++;
+        console.log(`${sRoundResult} \nPlayer Score: ${iPlayerScore}\tComputer Score: ${iComputerScore}`);
+    }
+    if (iComputerScore > iPlayerScore) console.log("🔴 Computer wins! 😢");
+    if (iComputerScore < iPlayerScore) console.log("👏 You win! 🐱‍🚀");
+    if (iComputerScore == iPlayerScore) console.log("💥 No way! It's a draw! 🤣");
+}
+// Play a game!
+sGame();
+//4.e end
+
+// Additional functionality: Wanna play another game? start
+let sPlayAnotherGame = prompt("Wanna play another one? 😎", "Type Yes or No..."); 
+
+while (sPlayAnotherGame.toLowerCase() == "yes") {
+    sGame();
+    sPlayAnotherGame = prompt("Wanna play another one? 😎", "Type Yes or No...");
+}
+// Additional functionality end
+
